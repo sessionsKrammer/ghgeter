3db6e2e0a0c63853bf33822233a99cfc00213c37
Alex Teisheira <fukirx@gmail.com>
2021-11-15 11:54:41 +0200

    some fetaure

--- src/components/hoc/withInputFetch.tsx
+++ src/components/hoc/withInputFetch.tsx
@@ -1,6 +1,11 @@
+import { assert } from "console";
 import React, { useState, FC } from "react";
 
 const withInputFetch = (Wrapped: FC<any>) => (props: any) => {
+  const a = 1;
+
+  const b = 2;
+
   return <Wrapped {...props} />;
 };
 
